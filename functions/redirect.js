a5fb1e9f049a8d58b7cc85ed4f37352402eb3ba7
Kent C. Dodds <kent@doddsfamily.us>
2018-12-05 14:14:37 -0700

    no-cache explicit

--- functions/redirect.js
+++ functions/redirect.js
@@ -68,6 +68,7 @@ exports.handler = async event => {
         // this needs to be enabled... but I'm really struggling on how to make
         // it work properly...
         // 'Cache-Control': 'public, max-age=10080', // 10080 seconds is 1 week
+        'Cache-Control': 'no-cache',
         // these headers I got by curling a bit.ly URL
         // and just doing what they do.
         'Content-Length': String(body.length),
