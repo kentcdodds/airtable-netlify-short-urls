50272ca2bd5a8a649fdde8993f4a3b4d4dfb014e
Kent C. Dodds <kent@doddsfamily.us>
2018-12-05 00:00:05 -0700

    fix shorten

--- other/shorten.js
+++ other/shorten.js
@@ -1,6 +1,9 @@
-require('dotenv').config()
+const path = require('path')
+const {URL} = require('url')
 const Airtable = require('airtable')
 const {copy} = require('copy-paste')
+
+require('dotenv').config({path: path.join(__dirname, '../.env')})
 
 const apiKey = getEnv('AIRTABLE_KEY')
 const base = getEnv('AIRTABLE_BASE')
@@ -13,11 +16,21 @@ const airtable = new Airtable({apiKey})
 let [, , longLink, code] = process.argv
 code = code || generateCode()
 
-console.log(`Attempting to set redirect "${code}" -> ${longLink}`)
-
 main()
 
 async function main() {
+  if (!longLink) {
+    console.log('Must provide the full link as an argument')
+    return
+  }
+  try {
+    // validate URL
+    new URL(longLink)
+  } catch (error) {
+    console.log(`${longLink} is not a valid URL`)
+    return
+  }
+  console.log(`Attempting to set redirect "${code}" -> ${longLink}`)
   const existingRecords = await getExistingRecord()
   if (existingRecords && existingRecords[0]) {
     const existingLink = existingRecords[0].get(longLinkField)
